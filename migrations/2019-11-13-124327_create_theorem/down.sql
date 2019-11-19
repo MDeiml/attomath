299212615c70682f2499ade8f56d9c7ac07ecd58
Matthias Deiml <matthias@deiml.net>
2019-11-19 15:20:45 +0100

    More cleanup

--- migrations/2019-11-13-124327_create_theorem/down.sql
+++
@@ -1,8 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP INDEX conclusion;
-DROP INDEX theorem_uniq;
-DROP INDEX theorem_new_uniq;
-DROP INDEX theorem_score;
-DROP INDEX theorem_score_new;
-DROP TABLE theorem;
-DROP TABLE theorem_new;
