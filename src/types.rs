87e55046fc7cc69d887897f43f403d50f5508069
MDeiml <matthias@deiml.net>
2019-11-21 10:40:49 +0100

    Updated documentation for types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,6 @@
-/// Type alias for a judgement (see [`Statement`](../statement/trait.Statement.html))
+/// Type alias for a judgement (see [`Statement`](../statement/struct.Statement.html))
 pub type Judgement = u16;
 
 /// Type alias for a identifier representing a variable or operator (see
-/// [`Statement`](../Statement/trait.Statement.html))
+/// [`Expression`](../expression/struct.Expression.html))
 pub type Identifier = i16;
